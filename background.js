b1c8cde8608ca1b797bd8991ef32699b8f7edeff
G2bb <g2bb@aol.com>
2022-02-27 17:30:31 -0600

    V6.0 - Pokemon Scarlet and Violet

--- background.js
+++ background.js
@@ -1,5 +1,9 @@
+//Set Badge color
+chrome.action.setBadgeBackgroundColor({color: "#8F00FF"});
+
 // Set vars
 var CDT, CUT, distance;
+var BadgeCDT;
 
 // Set the date we're counting down to
 var countDownDate = new Date("Nov 18, 2022 00:00:00").getTime();
@@ -35,11 +39,16 @@ setInterval(function() {
         }
     }
 
+    //Fetch Settings
+    chrome.storage.local.get("BadgeCDTSetting", function(result) {
+        BadgeCDT = result.BadgeCDTSetting;
+    });
+
     // Display the result in the banner text
-    if (distance > 0) {
-        console.log("D/P");
+    if (distance > 0 && BadgeCDT) {
         chrome.action.setBadgeText({text: badgeDay + "D"});
-        chrome.action.setBadgeBackgroundColor({color: "#8F00FF"});
+    } else {
+        chrome.action.setBadgeText({text: ""});
     }
 
 }, 1000);
--- popup.js
+++ popup.js
@@ -5,3 +5,81 @@ setInterval(function() {
         document.getElementById("CDT").innerHTML = "Pokemon Scarlet and Violet has been available for</br>" + CUT.days + " Days " + CUT.hours + " Hrs " + CUT.minutes + " Mins " + CUT.seconds + " Secs";
     }
 }, 1000);
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    //Make settings menu toggle buttons work.
+    var gear = document.getElementById('settingsBtn');
+    gear.addEventListener('click', function() {
+        toggleSettings();
+    });
+    var close = document.getElementById('SettingsCloseBtn');
+    close.addEventListener('click', function() {
+        toggleSettings();
+    });
+
+    //Save settings when anything changes.
+    var BadgeChkBx = document.getElementById('BadgeChkBx');
+    BadgeChkBx.addEventListener('click', function() {
+        storeData();
+    });
+    var RssChkBx = document.getElementById('RssChkBx');
+    RssChkBx.addEventListener('click', function() {
+        storeData();
+        toggleRss();
+    });
+
+    //Toggle settings menu "hidden" class.
+    function toggleSettings(){
+        var SettingsMenu = document.getElementById('SettingsMenu');
+        SettingsMenu.classList.toggle('hidden');
+    }
+
+    //Fill settings with curent values.
+    chrome.storage.local.get("BadgeCDTSetting", function(result) {
+        document.getElementById('BadgeChkBx').checked = result.BadgeCDTSetting;
+    });
+    chrome.storage.local.get("RssSetting", function(result) {
+        document.getElementById('RssChkBx').checked = result.RssSetting;
+    });
+
+    setTimeout(function(){
+        //When DOM loads check if RssChkBx is unchecked. If yes hide.
+        if(!RssChkBx.checked){
+            toggleRss();
+        }
+    }, 100);
+
+    //Toggle RSS based on sored setting.
+    function toggleRss(){
+        var Rss = document.getElementById('marquee');
+        var TimeBg = document.getElementById('TimeBG');
+        Rss.classList.toggle('hidden');
+        TimeBG.classList.toggle('noRSS');
+    }
+});
+
+
+
+function storeData(){
+    // Get a value saved in a form.
+    var BadgeCDT = document.getElementById('BadgeChkBx').checked;
+    var RssChkBx = document.getElementById('RssChkBx').checked;
+
+    // Save it using the Chrome extension storage API.
+    chrome.storage.local.set({'BadgeCDTSetting': BadgeCDT});
+    chrome.storage.local.set({'RssSetting': RssChkBx});
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const feedUrl = ['https://cors.g2bb1965.workers.dev/corsproxy/?apiurl=https://www.pokemon.com/us/pokemon-news/rss?order=DESC'
+    ];
+
+    const elementSelector = document.getElementById('marquee');
+
+    new RSSMarquee(feedUrl, elementSelector, {
+        speed: 110,
+        maxItems: 5
+    });
+});
